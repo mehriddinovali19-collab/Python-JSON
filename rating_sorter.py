7734748cff861493e11b7e9af1e8b37609e687f4
ali mekhridinov <mehriddinovali19@gmail.com>
2026-04-27 16:00:26 +0500

    task done

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,1 +1,10 @@
+import json
+input_file = open("students.json", "r")
+json01 = input_file.read()
 
+data = json.loads(json01)
+data.sort(key = lambda students: students['score'])
+
+output_file = json.dumps(data, indent = 4)
+f = open('rating.json', "w")
+f.write(output_file)
